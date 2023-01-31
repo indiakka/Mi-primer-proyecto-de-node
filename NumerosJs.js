879c985a647f41a78866bbd9efb4a043c0299782
indiakka <indiakka@gmail.com>
2023-01-31 17:38:33 +0100

    round por ceil

--- NumerosJs.js
+++ NumerosJs.js
@@ -2,6 +2,6 @@
 let altura = 163
 let alturaMetros =1.60
 let peso = 72.50
-let alturaRedondeadaArriba=Math.round(alturaMetros)
+let alturaRedondeadaArriba=Math.ceil(alturaMetros)
 let alturaRedondeadaAbajo=Math.floor(peso)
 let contenga = Number.MAX_VALUE +1 == Number.MAX_VALUE
